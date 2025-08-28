77b8cf513f09fdf629eec5d2d875e4cab37e421e
First edit <buitho@oregonstate.edu>
2025-08-27 19:43:02 -0700

    complete up to 15e of lesson 15 exercise

--- lesson-15-exercise.js
+++ lesson-15-exercise.js
@@ -1,4 +1,23 @@
-import dayjs from 'dayjs';
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
-const today = daysjs();
-console.log(today)
+const today = dayjs();
+console.log(today);
+const newDay = today.format("dddd")
+console.log(newDay);
+const fiveAhead = today.subtract(2, 'month')
+console.log(fiveAhead.format('MMMM, D, dddd'))
+
+console.log('hi')
+
+function isWeekend(date) {
+  //check if date given on Saturday or Sunday
+  const dayOfWeek = date.format("dddd")
+  if (dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday') {
+    //return true if it is, else false 
+    return true 
+  } else {
+    return false
+  };
+};
+
+console.log(isWeekend(fiveAhead));
